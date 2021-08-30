1b09f46d67e415da178949692853aad9ae0d49f2
Claudio Serpa <claudioserpa25@gmail.com>
2021-08-30 11:29:54 -0300

    Seeders 2

---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $contato = new SiteContato();
+        $contato->nome = 'Dillion Harper';
+        $contato->email = 'oculos@sh.com';
+        $contato->telefone = '12999777788';
+        $contato->motivo_contato = 1;
+        $contato->mensagem = 'Goza no meu oculos';
+        $contato->save();
+    }
+}
