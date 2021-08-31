cffc412236687e7ba88075163cd6d75657baec68
Claudio Serpa <claudioserpa25@gmail.com>
2021-08-31 08:41:32 -0300

    Aula 121

--- app/Http/Controllers/contatoController.php
+++ app/Http/Controllers/contatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class contatoController extends Controller
 {
@@ -10,6 +11,15 @@ class contatoController extends Controller
 
        //$request->all(); Todo data
        //$request->input('nome'); Dado especifico
+
+       $contato = new SiteContato();
+       $contato->nome = $request->input('nome');
+       $contato->telefone = $request->input('telefone');
+       $contato->email = $request->input('email');
+       $contato->motivo_contato = $request->input('motivo_contato');
+       $contato->mensagem = $request->input('mensagem');
+       print_r($contato->getAttributes());
+       $contato->save();
         return view('site.contato');
     }
 }
